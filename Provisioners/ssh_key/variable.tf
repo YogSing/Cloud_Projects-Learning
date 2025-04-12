4d69684539ca9123ef6f81c7715ef33cf0288526
YogSing <85532745+YogSing@users.noreply.github.com>
2025-04-12 01:36:36 -0300

    Add files via upload

---
+++ Provisioners/ssh_key/variable.tf
@@ -0,0 +1,15 @@
+variable "rg" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "rg_id" {
+  description = "The ID of the resource group"
+  type        = string
+
+}
+
+variable "rg_location" {
+  description = "value of the location"
+  type        = string
+}
