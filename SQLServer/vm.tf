d4b4b7b13786325b8111a59612a4be246291fa28
YogSing <85532745+YogSing@users.noreply.github.com>
2025-04-13 21:40:54 -0300

    Add files via upload

---
+++ SQLServer/vm.tf
@@ -0,0 +1,133 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "SQL-Server-RG1"
+  location = "East US"
+  tags = {
+    "environment" = "testing"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "Virtual-Network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "Virtual-Network-Subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet1-nsg" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "SQL-Server-Network-Security-Group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_network_security_rule" "SQLrule" {
+  name                        = "Allow-SQL-Server"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1433"
+  source_address_prefix       = "Source IP"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "RDPrule" {
+  name                        = "Allow-RDP"
+  priority                    = 201
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "Source IP"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+
+}
+
+
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "SQL-Server-Public-IP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "Virtual-nic1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg-association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "sql-virtual-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "Password1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  disable_password_authentication = false
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "microsoftsqlserver"
+    offer     = "sql2019-ubuntu2004"
+    sku       = "sqldev"
+    version   = "latest"
+  }
+}
+
+
+resource "azurerm_mssql_virtual_machine" "mssql" {
+  virtual_machine_id               = azurerm_linux_virtual_machine.vm.id
+  sql_license_type                 = "PAYG"
+  r_services_enabled               = true
+  sql_connectivity_port            = 1433
+  sql_connectivity_type            = "PRIVATE"
+  sql_connectivity_update_password = "Password1234!"
+  sql_connectivity_update_username = "sqllogin"
+
+
+}
+
+
+resource "azurerm_mssql_database" "database" {
+  name      = "sampledb"
+  server_id = azurerm_mssql_virtual_machine.mssql.id
+}
+
