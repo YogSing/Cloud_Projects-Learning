27e907f1c3bba11451c193f47b270e9b8a7ff200
YogSing <85532745+YogSing@users.noreply.github.com>
2025-04-18 21:45:24 -0300

    Add files via upload

---
+++ Azure_Monitor/stress_cmd.sh
@@ -0,0 +1,10 @@
+sha1sum /dev/zero &
+commands for every core in your system.
+
+To stop killall sha1sum
+or
+Copy kill sha1sum
+# This script will run sha1sum on /dev/zero in the background
+# for every core in your system.
+# It will run until you kill it.
+# It will use all the CPU cores available.
